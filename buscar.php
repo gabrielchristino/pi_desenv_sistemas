1069069cd5600cd51ffeea79fabd182f695b6b29
gabrielchristino <gabriel.christino@gmail.com>
2022-11-16 18:12:01 -0300

    fix: botoes de pesquisa

--- buscar.php
+++ buscar.php
@@ -5,15 +5,18 @@ include('cabecalho.php');
 <h2>Buscar veículos</h2>
 <div class="formularioPesquisa">
             <form  method="post">
-                <input type="text" id="localizacao" name="localizacao" class="pesquisaCidade" placeholder="Qual Municipio-UF de retirada do veiculo?">
-                <div class="botoesBuscar">
+            <div class="caixaPesquisa">
+            <input type="text" id="localizacao" name="localizacao" class="pesquisaCidade" placeholder="Qual Municipio-UF de retirada do veiculo?">
+            <img src="./images/iconepesquisa.svg" alt="">
+            </div>
+            <div class="botoesBuscar">
                     <button class="botaoInicial" type="submit" id="submitBuscaVeiculo" name="acao">
                         <img src="./images/botaocarro.svg" alt="">
-                        <p>Busco Veículos para alugar</p>
+                        <p>Carros e Utilitários</p>
                     </button>
                     <button class="botaoInicial" type="submit" id="submitBuscaVeiculo" name="acao">
-                        <img src="./images/botaoanunciar.svg" alt="">
-                        <p>Quero anunciar meu veículo para locação!</p>
+                        <img src="./images/botaomoto.svg" alt="">
+                        <p>Motos</p>
                     </button>
                 </div>
             </form>
