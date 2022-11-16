d2c75f591ba8c712471f969404597b2704952358
gabrielchristino <gabriel.christino@gmail.com>
2022-11-16 13:28:06 -0300

    feat: tela home e rodape-menu

--- home.php
+++ home.php
@@ -1,8 +1,18 @@
-
-        <?php
-            include('cabecalho.php');
-        ?>
 <?php
-echo '<h2>Bem vindo ' . $_SESSION['login'] . '</h2>';
-echo '<a href="?logout">Sair!</a>';
+echo '<a class="btnSair" href="?logout">sair</a>';
+include('cabecalho.php');
+echo '<h4 class="cabecalhoHome">Bem vindo ' . $_SESSION['login'] . '</h4>';
+?>
+<div class="botoesInicial">
+    <button class="botaoInicial">
+        <img src="./images/botaocarro.svg" alt="">
+        <p>Busco Veículos para alugar</p>
+    </button>
+    <button class="botaoInicial">
+        <img src="./images/botaoanunciar.svg" alt="">
+        <p>Quero anunciar meu veículo para locação!</p>
+    </button>
+</div>
+<?php
+include('menu.php');
 ?>
---
+++ menu.php
@@ -0,0 +1,17 @@
+<div class="rodape">
+    <button class="botaoMenu">
+        <img src="./images/botaohome.svg" alt="">
+    </button>
+    <button class="botaoMenu">
+        <img src="./images/botaolupa2.svg" alt="">
+    </button>
+    <button class="botaoMenu">
+        <img src="./images/botaoanunciar2.svg" alt="">
+    </button>
+    <button class="botaoMenu">
+        <img src="./images/botaocalendario.svg" alt="">
+    </button>
+    <button class="botaoMenu">
+        <img src="./images/botaoperfil.svg" alt="">
+    </button>
+</div>
